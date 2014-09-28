3a07b9201e9aedac6fa0610f0ca0dde1f853225c
Dirk Boye <dirkboye@gmail.com>
2014-09-28 12:56:45 +0200

    Additional Content-Type header information for browser access on root of gl.py

--- gl.py
+++ gl.py
@@ -9,4 +9,13 @@ os.environ['GIT_PROJECT_ROOT'] = home_dir + '/repositories'
 os.environ['GITOLITE_HTTP_HOME'] = home_dir
 os.environ['GIT_HTTP_BACKEND'] = '/usr/libexec/git-core/git-http-backend'
 os.environ['GIT_HTTP_EXPORT_ALL'] = ''
-subprocess.call("/usr/bin/gl-auth-command")
+path = os.environ['PATH_INFO']
+if (path == "/"):
+        # Add additional content type header so the information is displayed in the browser
+        # This only works for text output from gl-auth-command and cannot be user for git streams
+        print "Content-Type: text-html"
+        p = subprocess.Popen("/usr/bin/gl-auth-command", stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        print out
+else:
+        subprocess.call("/usr/bin/gl-auth-command")
